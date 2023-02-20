d525a1cee0105b5cda94d174521b0da4eac99abc
najam <sameerfromfsd@gmail.com>
2023-02-20 11:59:22 +0500

    18 feb

---
+++ src/Material-Module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import {MatAutocompleteModule} from '@angular/material/autocomplete';
+
+@NgModule({
+
+    export:[
+        MatAutocompleteModule,
+    ]
+})
+
+export class MaterialModule{
+
+}
