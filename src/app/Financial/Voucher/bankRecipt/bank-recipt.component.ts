c18624f681ff5b0f76ee136cfae798a708699427
sameerfromfsd <sameerfromfsd@gmail.com>
2023-06-24 07:24:53 -0700

    24june financial All components struct

---
+++ src/app/Financial/Voucher/bankRecipt/bank-recipt.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'bank-recipt',
+  templateUrl: './bank-recipt.component.html',
+  styleUrls: ['./bank-recipt.component.css']
+})
+export class BankReciptComponent {
+
+}
